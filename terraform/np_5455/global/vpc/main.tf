97a9c897be99f2dfb6c94db316bf97f5ade605db
miles.lee <miles.lee@nsuslab.com>
2024-01-12 19:54:03 +0900

    test

--- terraform/np_5455/global/vpc/main.tf
+++ terraform/np_5455/global/vpc/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket         = "nsuslab-devops-terraform-state"
-    key            = "digger/np_5455/global/vpc/terraform.tfstate"
+    # key            = "digger/np_5455/global/vpc/terraform.tfstate"
     region         = "ap-northeast-2"
     encrypt        = false
     dynamodb_table = "nsuslab-devops-lock-table"
@@ -28,7 +28,7 @@ provider "aws" {
 resource "aws_vpc" "vpc_network" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "terraform-network-np"
+    Name = "terraform-network-np2"
   }
 }
 
